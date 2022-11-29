c4f25f30da6ee423d405ab5fcddd7dd985856f5f
SlavKanev <vilikanev@gmail.com>
2022-11-29 16:17:59 +0200

    Added controllers,models,views for all the tabs in the header. No functionality. Login is completed.

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,23 @@
+﻿using FootballWcFacts.Core.Models.Author;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballWcFacts.Controllers
+{
+    [Authorize]
+    public class AuthorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Become()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Become(BecomeAuthorModel model)
+        {
+            return RedirectToAction("All", "Fact");
+        }
+
+    }
+}
--- Controllers/FactController.cs
+++ Controllers/FactController.cs
@@ -1,14 +1,68 @@
 ﻿using FootballWcFacts.Core.Models.Fact;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FootballWcFacts.Controllers
 {
+    [Authorize]
     public class FactController : Controller
     {
-        public IActionResult All()
+        [AllowAnonymous]
+        public async Task<IActionResult> All()
         {
-            var model = new AllFactsQueryModel();
+            var model = new FactsQueryModel();
             return View(model);
         }
+        
+        public async Task<IActionResult> Mine()
+        {
+            var model = new FactsQueryModel();
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Details()
+        {
+            var model = new FactDetailsModel();
+            return View(model);
+        }
+        
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Add(FactModel model)
+        {
+            int id = 1;
+
+            return RedirectToAction(nameof(Details), new { id });
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = new FactModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, FactModel model)
+        {
+            return RedirectToAction(nameof(Details), new { id });
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return RedirectToAction(nameof(All));
+        }
+
+
+
     }
 }
