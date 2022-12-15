04d2f56cbe6626228c889f868e4c22a86ef5e55c
SlavKanev <vilikanev@gmail.com>
2022-12-15 16:32:03 +0200

    area admin added

---
+++ Areas/Admin/Constants/AdminConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace FootballWcFacts.Areas.Admin.Constants
+{
+    public static class AdminConstants
+    {
+
+        public const string AreaName = "Admin";
+
+        public const string AdminRoleName = "Administrator";
+
+        public const string AdminEmail = "admin@fact.net";
+
+
+
+    }
+}
---
+++ Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using static FootballWcFacts.Areas.Admin.Constants.AdminConstants;
+
+namespace FootballWcFacts.Areas.Admin.Controllers
+{
+    [Area(AreaName)]
+    [Route("Admin/[controller]/[Action]/{id?}")]
+    [Authorize(Roles = AdminRoleName)]
+    public class BaseController : Controller
+    {
+    }
+}
